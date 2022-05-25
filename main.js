ade74579f7dfff53d7897a73cb71a10b988800a9
Hammad-24 <104418073+Hammad-24@users.noreply.github.com>
2022-05-25 12:24:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ leftWristX = 0;
   video = createCapture(VIDEO);
   video.size(550, 500);
 
-  canvas = createCanvas(550, 550);
-  canvas.position(560,150);
+  canvas = createCanvas(500, 450);
+  canvas.position(560,100);
 
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
